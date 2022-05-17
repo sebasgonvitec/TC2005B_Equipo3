947142047722f3f7b0399574dcec92c4f5feb3bb
karla-mond <karla.mondragon.rosas@gmail.com>
2022-05-17 12:29:20 -0500

    Actualizaciones Frontend

--- Frontend/js/scripts.js
+++ Frontend/js/scripts.js
@@ -1,11 +1,4 @@
-/*!
-* Start Bootstrap - Grayscale v7.0.5 (https://startbootstrap.com/theme/grayscale)
-* Copyright 2013-2022 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
+// Script
 
 window.addEventListener('DOMContentLoaded', event => {
 
