06d6d2edd4314d5256cb904ea43df6060552621c
karla-mond <karla.mondragon.rosas@gmail.com>
2022-06-06 13:49:20 -0500

    correcciones importantes

--- Database/consultas.sql
+++ Database/consultas.sql
@@ -5,12 +5,18 @@ SELECT * FROM levels;
 SELECT * FROM ratings;
 SELECT * FROM gameplays;
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 SELECT num_levels_created FROM users;
 =======
+=======
+>>>>>>> Stashed changes
 SELECT * FROM gameplays;
 
 DESCRIBE users;
 DESCRIBE levels;
 DESCRIBE gameplays;
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 
